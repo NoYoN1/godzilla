16afad4cdbde80d4e27428dda08225c11097012d
NoYoN1 <uilse1122@gmail.com>
2021-11-09 15:04:12 +0900

    2021.11.09

--- static/js/app.js
+++
@@ -1,7 +0,0 @@
-// function hello_world() {
-//   document.getElementById("demo").innerHTML = "Hello world from javascript";
-// }
-
-// document.getElementById("demo").innerHTML = "Hello world from javascript";
-
-document.write("Hello world from javascript ");
--- web_page.py
+++ web_page.py
@@ -5,13 +5,16 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-# import web_data
+from flask import Markup
+import web_data
 # import do_trade
+import py.EdgeStudy as EdgeStudy
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+
     return (render_template('index.html'))
 
 # @app.route('/')
@@ -31,20 +34,20 @@ def index():
 #         return "<h1>文字列は違います !</h1>"
 
 
-# @app.route('/object', )
-# def print_object_data():
-#     # return str(web_data.p1.name) + " is " + str(web_data.p1.age) + " years old"
+@app.route('/object', )
+def print_object_data():
+    # return str(web_data.p1.name) + " is " + str(web_data.p1.age) + " years old"
 
-#     # for i in web_data.m:
-#     #     return str(i.name)
+    # for i in web_data.m:
+    #     return str(i.name)
 
-#     return render_template('object.html', web_data=web_data)
+    return render_template('object.html', web_data=web_data)
 
 
-# @app.route('/trade', )
-# def print_trade():
+@app.route('/trade',)
+def print_trade():
 
-#     return render_template('trade.html', do_trade=do_trade)
+    return (render_template('trade.html', EdgeStudy=EdgeStudy))
 
 
 # @app.route('/test', )
@@ -53,6 +56,30 @@ def index():
 #     return render_template('test.html', do_trade=do_trade.aaa)
 
     # return str(do_trade.aaa)
+
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
+
+@app.route('/bar')
+def bar():
+    bar_labels = labels
+    bar_values = values
+    return render_template('bar_chart.html', title='Bitcoin Monthly Price in USD', max=17000, labels=bar_labels, values=bar_values)
 
 
 if __name__ == '__main__':

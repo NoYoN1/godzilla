24503b0b6211cf479a70f27ef4987d433a8d390c
NoYoN1 <uilse1122@gmail.com>
2021-11-19 23:29:30 +0900

    get data from python. show display final result in risk management page

---
+++ py_files/sum_test.py
@@ -0,0 +1,16 @@
+
+
+class Sumsuru:
+    def __init__(self, a1, b1):
+        self.a = a1
+        self.b = b1
+
+    def sum_t(self):
+
+        return self.a + self.b
+
+    def sum_a(self):
+        aaa = self.a + self.b
+        bbb = 2 + 4
+
+        return aaa, bbb
---
+++ py_files/sum_test1.py
@@ -0,0 +1,26 @@
+# import sum_test
+
+# t = sum_test.Sumsuru()
+# print(t.sum_t(2, 2))
+
+# Class1
+# class Test:
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+
+#     def calc(self):
+
+#         return self.a + self.b
+
+# # Class 2
+# class Test2:
+
+import sum_test
+obj = sum_test.Sumsuru(1, 2)
+print(obj.sum_t())
+print(sum_test.Sumsuru(2, 2).sum_t())
+
+aaa = sum_test.Sumsuru(2, 3).sum_a()
+r1 = aaa[0]
+print(r1)

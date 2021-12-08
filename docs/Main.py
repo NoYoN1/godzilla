2b0c2d7f9c642488d08b4b5539d2382761086e96
NoYoN1 <uilse1122@gmail.com>
2021-12-08 16:24:08 +0900

    create docs director

---
+++ docs/Main.py
@@ -0,0 +1,109 @@
+# 2021-10-10
+#
+# 卒業作品
+#
+from re import S, template
+from flask import Flask
+from pandas.io import json
+from flask import request
+from flask import render_template
+from flask import Markup
+import py_files.web_data
+# import py.coin1 as coin
+import py.EdgeStudy as EdgeStudy
+app = Flask(__name__)
+
+
+@app.route('/',)
+def index():
+
+    return (render_template('index.html'))
+
+
+@app.route('/test')
+def my_form():
+
+    return (render_template('test.html'))
+
+
+# @app.route('/object', )
+# def print_object_data():
+#     # return str(web_data.p1.name) + " is " + str(web_data.p1.age) + " years old"
+
+#     # for i in web_data.m:
+#     #     return str(i.name)
+
+#     return render_template('object.html', web_data=web_data)
+
+
+@app.route('/risk-management', methods=["GET", "POST"])
+def riskmanagement():
+    if request.method == "POST":
+        title = "GODZILLA TRADE STRATEGY"
+        # getting input with name = initialCash in HTML form
+        initialCash = request.form.get("initialCash", type=float)
+        # getting input with name = tradesRequired in HTML form
+        tradesRequired = request.form.get("tradesRequired", type=int)
+        # getting input with name = riskPerTrade in HTML form
+        riskPerTrade = request.form.get("riskPerTrade", type=float)
+        # getting input with name = profitRatio in HTML form
+        profitRatio = request.form.get("profitRatio", type=float)
+        # getting input with name = winRatio in HTML form
+        winRatio = request.form.get("winRatio", type=float)
+        # getting input with name = dataRequired in HTML form
+        dataRequired = request.form.get("dataRequired", type=int)
+
+        # set value EdgeStudy.py
+        result = EdgeStudy.Simulate(initialCash, tradesRequired,
+                                    riskPerTrade, profitRatio, winRatio)
+        getMultiChartData = result.multiple(initialCash, tradesRequired,
+                                            riskPerTrade, profitRatio, winRatio)
+
+        final_result = result.printFinal()
+        # get single data
+        chartData = result.printTrade(0)[1]
+        # get multi data result
+        dataMulti1 = [*range(1, 10, 1)]
+
+        mm = [*range(1, 6, 1)]
+        for m in range(1, 5):
+            mm[m] = getMultiChartData[m]
+
+        # get multi data
+        dataMulti = {}
+        for k in range(dataRequired):
+            for v in getMultiChartData[0]:
+                dataMulti[k] = v
+                getMultiChartData[0].remove(v)
+                break
+        jsMulti = json.dumps(dataMulti)
+        file = open('static/json/chartMulti.json', 'w')
+        file.write(jsMulti)
+        file.close()
+
+        # data = []
+        # for i in range(1):
+        #     data = {i: chartData, }
+        # js = json.dumps(data)
+        # file = open('static/json/chart.json', 'w')
+        # file.write(js)
+        # file.close()
+
+        return (render_template('/risk/risk.html', title=title, initialCash=initialCash, tradesRequired=tradesRequired, riskPerTrade=riskPerTrade, profitRatio=profitRatio, winRatio=winRatio, final_result=final_result, chartData=chartData, mm=mm, dataMulti1=dataMulti1))
+    return render_template('/index.html',)
+
+
+@ app.route('/chart')
+def chart_html():
+
+    return (render_template('/risk/chart.html', ))
+
+
+@ app.route('/trade')
+def trade_html():
+    print(b)
+    return (render_template('trade.html',))
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5550, debug=True)
---
+++ docs/py/EdgeStudy.py
@@ -0,0 +1,237 @@
+import random
+from colorama import Fore
+import matplotlib.pyplot as plt
+
+"""
+Trading Strategy Edge Simulator
+
+1. Simulates trades based on RPT and Edge.
+
+"""
+
+
+class Simulate:
+    def __init__(self, initialCash, tradesRequired,
+                 riskPerTrade, profitRatio, winRatio):
+
+        # self.printTradeTrue = True
+        # Initial_cash = 10000  # Initial Cash
+        # self.cash = Initial_cash
+        # self.tradesRequired = 100
+        # self.rpt = 1 / 100  # 1/100 = 1% Risk per Trade
+        # self.profitRatio = 1  # 2 = 2:1 Profit Ratio
+        # self.winRate = 50  # Percentage. Win Ratio, 50 = 50% win rate
+
+        self.printTradeTrue = True
+        Initial_cash = initialCash  # Initial Cash
+        self.cash = Initial_cash
+        self.tradesRequired = tradesRequired
+        self.rpt = riskPerTrade / 100  # 1/100 = 1% Risk per Trade
+        self.profitRatio = profitRatio  # 2 = 2:1 Profit Ratio
+        self.winRate = winRatio  # Percentage. Win Ratio, 50 = 50% win rate
+
+        self.money = []
+        self.tradeIndex = []
+        self.portfolio = 0
+        self.trades = 0
+        self.printTimes = 0
+
+        self.pnlNet = 0
+        self.tradeIsWin = False
+        self.win = 0
+        self.loss = 0
+        self.strikeRate = 0
+        self.winStreak = 0
+        self.currentWinStreak = 0
+        self.lossStreak = 0
+        self.currentLossStreak = 0
+        self.positionChange = 0
+        self.grossProfit = 0
+        self.grossLoss = 0
+
+        self.ATLow = self.cash
+        self.ATHigh = self.cash
+        self.ATLowPos = 0
+        self.ATHighPos = 0
+        self.portfolioPeak = 0
+        self.portfolioLow = 999
+        self.positionPeak = 0
+        self.positionLow = 0
+        self.drawdown = 0
+        self.maxDrawdown = 0
+        self.maxDDPeakPos = 0
+        self.maxDDLowPos = 0
+
+    def printTrade(self, i):
+        if self.positionChange > 0:
+            print(Fore.GREEN + 'Trade %.0f = $%.2f || $%.2f' %
+                  (self.trades, self.money[i], self.positionChange))
+        else:
+            print(Fore.RED + 'Trade %.0f = $%.2f || $%.2f ' %
+                  (self.trades, self.money[i], self.positionChange,))
+        return(self.trades, self.money)
+
+    def printSetup(self, Initial_cash):
+        print(Fore.CYAN)
+        print('Initial Cash     : $%.f' % Initial_cash)
+        print('Number of Trades : %.f' % self.tradesRequired)
+        print('Risk Per Trade   : %.f' % (self.rpt*100) + '%')
+        print('Profit Ratio     : %.f' % self.profitRatio + ':1')
+        print('Win Rate         : %.f' % self.winRate + '%')
+
+    def printFinal(self,):
+        # print(Fore.RESET)
+        # print('Final Cash  : $%.2f' % self.cash)
+        # print('Gross Profit: $%.f' % self.grossProfit)
+        # print('Gross Loss  : $%.f' % self.grossLoss)
+        # print('PnL Net     : %.2f' % self.pnlNet + '%')
+        # print('Strike Rate : %.1f' % self.strikeRate)
+        # print('Win         : %.f ||   Loss         : %.f' %
+        #       (self.win, self.loss))
+        # print('Win Streak  : %.f   ||   Loss Streak  : %.f' %
+        #       (self.winStreak, self.lossStreak))
+        # print('Max Drawdown: %.3f' % self.maxDrawdown + '%')
+        # print('Max Drawdown Peak: %.f || Max Drawdown Low: %.f' %
+        #       (self.maxDDPeakPos, self.maxDDLowPos))
+        # print('Max All Time High: $%.f || Max All time Low: $%.2f' %
+        #       (self.ATHigh, self.ATLow))
+        # print('Max ATLOWPOS: %.f' % self.ATLowPos)
+        return self.cash, self.grossProfit, self.grossLoss, self.pnlNet, self.strikeRate, self.win, self.loss, self.winStreak, self.lossStreak, self.maxDrawdown, self.maxDDPeakPos, self.maxDDLowPos, self.ATHigh, self.ATLow, self.ATLowPos
+
+    def trade(self, i):
+        dice = random.randint(1, 100)
+        self.positionChange = 0
+
+        if dice <= self.winRate:
+            self.tradeIsWin = True
+        elif dice >= self.winRate:
+            self.tradeIsWin = False
+
+        if self.tradeIsWin:
+            self.positionChange += self.cash * self.rpt * self.profitRatio
+        elif not self.tradeIsWin:
+            self.positionChange -= self.cash * self.rpt
+
+        self.cash += self.positionChange
+
+        self.trades += 1
+        self.money.append(self.cash)
+        self.tradeIndex.append(i)
+        self.basicAnalyzer(i)
+
+        if self.printTradeTrue:
+            self.printTrade(i)
+
+    # def next(self, Initial_cash):
+    #     # print(f"Starting Cash: ${Initial_cash}")
+    #     # print()
+    #     while self.trades < self.tradesRequired:
+    #         self.trade(self.trades)
+
+        # self.printSetup(Initial_cash)
+        # self.printFinal()
+        # plt.plot(self.tradeIndex, self.money,)
+        # plt.ylabel('Money')
+        # plt.show()
+
+    def multiple(self, initialCash, tradesRequired,
+                 riskPerTrade, profitRatio, winRatio):
+
+        portfolioIndex = []
+        multi = []
+        cashR = []
+        pnlR = []
+        strikeR = []
+        maxR = []
+        self.printSetup(initialCash)
+        print(Fore.RESET)
+        for i in range(0, 10):
+            self.__init__(initialCash, tradesRequired,
+                          riskPerTrade, profitRatio, winRatio)
+            self.printTradeTrue = False
+            while self.trades < self.tradesRequired:
+                self.trade(self.trades)
+            portfolioIndex.append(self.money)
+            print('Final Cash #%.f  : $%.2f || PnL Net: %.2f || Strike Rate: %.1f || MAX DD: %.2f' %
+                  (i + 1, self.cash, self.pnlNet, self.strikeRate, self.maxDrawdown))
+            plt.plot(self.tradeIndex, portfolioIndex[i], )
+            multi.append(portfolioIndex[i])
+            cashR.append(self.cash)
+            pnlR.append(self.pnlNet)
+            strikeR.append(self.strikeRate)
+            maxR.append(self.maxDrawdown)
+        return (multi, cashR, pnlR, strikeR, maxR)
+
+        # plt.ylabel('Money')
+        # plt.show()
+
+    def basicAnalyzer(self, i):
+        # Gross Profit / Loss
+        if self.positionChange > 0:
+            self.grossProfit += self.positionChange
+        if self.positionChange < 0:
+            self.grossLoss += self.positionChange
+
+        # Win/Lose Streaks
+        if self.tradeIsWin:
+            self.currentWinStreak += 1
+
+            if self.currentLossStreak > self.lossStreak:
+                self.lossStreak = self.currentLossStreak
+            self.currentLossStreak = 0
+        elif not self.tradeIsWin:
+            self.currentLossStreak += 1
+
+            if self.currentWinStreak > self.winStreak:
+                self.winStreak = self.currentWinStreak
+            self.currentWinStreak = 0
+
+        # Cumulative
+        self.pnlNet = (self.cash-100000)/100000 * 100
+
+        # Strike Rate
+        if self.tradeIsWin:
+            self.win += 1
+        else:
+            self.loss += 1
+        self.strikeRate = (self.win/(self.win+self.loss)) * 100
+
+        # All Time High and Low
+        if self.money[i] > self.ATHigh:
+            self.ATHigh = self.money[i]
+        if self.money[i] < self.ATLow:
+            self.ATLow = self.money[i]
+            self.ATLowPos = i+1
+        # Drawdowns
+        if self.money[i] > self.portfolioPeak:
+            self.portfolioPeak = self.money[i]
+            self.positionPeak = i
+            self.portfolioLow = 100000000000000000000
+            self.positionLow = 0
+        if self.money[i] <= self.portfolioLow:
+            self.portfolioLow = self.money[i]
+            self.positionLow = i
+
+        self.drawdown = (
+            (self.portfolioPeak - self.portfolioLow)/self.portfolioPeak)*100
+        if self.drawdown > self.maxDrawdown:
+            self.maxDrawdown = self.drawdown
+            self.maxDDPeakPos = self.positionPeak+1
+            self.maxDDLowPos = self.positionLow+1
+
+
+# s1 = Simulate(initialCash, tradesRequired, riskPerTrade, profitRatio, winRatio)
+# # s1.printFinal()
+# Initial_cash = s1.cash
+# select = True
+# if select == True:
+#     s1.multiple(Initial_cash)
+# else:
+#     s1.next(Initial_cash)
+# aaa = s1.cash
+# bbb = Initial_cash
+
+# print(aaa)
+# print(bbb)
+
+# s1.multiple(Initial_cash)

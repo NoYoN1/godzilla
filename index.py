4e9ef8135e46db66376f349d75feabf3a6607945
NoYoN1 <uilse1122@gmail.com>
2023-01-15 13:23:19 +0900

    upload local server

--- index.py
+++ index.py
@@ -332,4 +332,5 @@ def normal_strategy_result():
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5550, debug=True)
+    # app.run(host="127.0.0.1", port=5550, debug=True)
+    app.run(debug=False, host='0.0.0.0')
